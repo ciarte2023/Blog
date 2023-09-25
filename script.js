2eaa1a9f358a0a8d2dbfcc49e4cb49bc6920c18f
levis <133056908+levis2229@users.noreply.github.com>
2023-09-25 19:14:25 -0300

    .

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var links = document.querySelectorAll("a[href^='#']");
+    
+    links.forEach(function (link) {
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Impede o comportamento padrão do link
+
+            var targetId = this.getAttribute("href").substring(1);
+            var targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                var headerHeight = document.querySelector("header").offsetHeight; // Altura do cabeçalho
+                var marginToHeader = 100;
+                var targetOffset = targetElement.offsetTop - headerHeight - marginToHeader;
+                var animationDuration = 1000;
+
+                window.scrollTo({
+                    top: targetOffset,
+                    behavior: "smooth",
+                    duration: animationDuration
+                });
+            }
+        });
+    });
+});
